ccca34355f11819b83b8988138e58d3d06407651
hzqsns <787624681@qq.com>
2022-09-13 17:15:47 +0800

    finish tcp_receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -5,15 +5,65 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// template <typename... Targs>
+// void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
+//! \brief handle an inbound segment
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // DUMMY_CODE(seg);
+    TCPHeader _tcp_header = seg.header();
+    // LISTEN：等待 SYN 包的到来。若在 SYN 包到来前就有其他数据到来，则必须丢弃。
+    if (!_set_syn_flag) {
+        if (!_tcp_header.syn) {
+            return;
+        }
+        _isn = _tcp_header.seqno;
+        _set_syn_flag = true;
+    }
+
+    // SYN_RECV：获取到了 SYN 包，此时可以正常的接收数据包
+
+    uint64_t _absolute_seqno = unwrap(_tcp_header.seqno, _isn, _reassembler.stream_out().bytes_written());
+    uint64_t _stream_index = _tcp_header.syn ? 0 : _absolute_seqno - 1;
+    // 此时没有被return的话我们继续往下进行判断，有可能接收到fin包
+    // FIN_RECV：获取到了 FIN 包，此时务必终止 ByteStream 数据流的输入。
+    if (_tcp_header.fin)
+        _set_fin_flag = true;
+    _reassembler.push_substring(seg.payload().copy(), _stream_index, _tcp_header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+//! \name Accessors to provide feedback to the remote TCPSender
+//!@{
 
-size_t TCPReceiver::window_size() const { return {}; }
+//! \brief The ackno that should be sent to the peer
+//! \returns empty if no SYN has been received
+//!
+//! This is the beginning of the receiver's window, or in other words, the sequence number
+//! of the first byte in the stream that the receiver hasn't received.
+//  这是接收方窗口的开始，或者换句话说，这是接收方尚未收到的流中第一个字节的序列号。
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_set_syn_flag)
+        return {};
+    if (_set_fin_flag && _reassembler.stream_out().input_ended()) {
+        return wrap(_reassembler.stream_out().bytes_written() + 2, _isn);
+    }
+
+    return wrap(_reassembler.stream_out().bytes_written() + 1, _isn);
+}
+
+//! \brief The window size that should be sent to the peer
+//!
+//! Operationally: the capacity minus the number of bytes that the
+//! TCPReceiver is holding in its byte stream (those that have been
+//! reassembled, but not consumed).
+//! 操作上：容量减去TCPReceiver在其字节流中持有的字节数（那些已经被重新组装但未被消耗的字节）。
+//  重新组装，但没有被消耗）。
+//! Formally: the difference between (a) the sequence number of
+//! the first byte that falls after the window (and will not be
+//! accepted by the receiver) and (b) the sequence number of the
+//! beginning of the window (the ackno).
+//  形式上：(a)窗口之后的第一个字节的序列号（不会被接收方接受）与(b)窗口开始的序列号（ackno）之间的差。(b)
+//  窗口开始的序列号（ackno）。
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -13,19 +13,26 @@
 //! Receives and reassembles segments into a ByteStream, and computes
 //! the acknowledgment number and window size to advertise back to the
 //! remote TCPSender.
+//  接收并将片段重新组合成ByteStream，并计算出确认号码和窗口大小，以回馈给远程TCPSender。
 class TCPReceiver {
     //! Our data structure for re-assembling bytes.
     StreamReassembler _reassembler;
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    //! 给包设置的 isn编号
+    WrappingInt32 _isn;
+    bool _set_syn_flag;
+    bool _set_fin_flag;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    //  接收器在任何时候都会在其缓冲区内存储的最大字节数。
+    TCPReceiver(const size_t capacity)
+        : _reassembler(capacity), _capacity(capacity), _isn(0), _set_syn_flag(false), _set_fin_flag(false) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
@@ -35,6 +42,7 @@ class TCPReceiver {
     //!
     //! This is the beginning of the receiver's window, or in other words, the sequence number
     //! of the first byte in the stream that the receiver hasn't received.
+    //  这是接收方窗口的开始，或者换句话说，这是接收方尚未收到的流中第一个字节的序列号。
     std::optional<WrappingInt32> ackno() const;
 
     //! \brief The window size that should be sent to the peer
@@ -42,15 +50,19 @@ class TCPReceiver {
     //! Operationally: the capacity minus the number of bytes that the
     //! TCPReceiver is holding in its byte stream (those that have been
     //! reassembled, but not consumed).
-    //!
+    //! 操作上：容量减去TCPReceiver在其字节流中持有的字节数（那些已经被重新组装但未被消耗的字节）。重新组装，但没有被消耗）。
     //! Formally: the difference between (a) the sequence number of
     //! the first byte that falls after the window (and will not be
     //! accepted by the receiver) and (b) the sequence number of the
     //! beginning of the window (the ackno).
+    //  形式上：(a)窗口之后的第一个字节的序列号（不会被接收方接受）与(b)窗口开始的序列号（ackno）之间的差。(b)
+    //  窗口开始的序列号（ackno）。
+
     size_t window_size() const;
     //!@}
 
     //! \brief number of bytes stored but not yet reassembled
+    //  已存储但尚未重新组装的字节数
     size_t unassembled_bytes() const { return _reassembler.unassembled_bytes(); }
 
     //! \brief handle an inbound segment
